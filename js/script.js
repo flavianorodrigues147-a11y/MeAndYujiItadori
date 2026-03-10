7ea594822857c15558ccb3f4ec397a65e6987013
flavianorodrigues147-a11y <flavianorodrigues147@gmail.com>
2026-03-09 21:50:21 -0300

    uma

--- js/script.js
+++ js/script.js
@@ -19,3 +19,11 @@ document.querySelectorAll(".anterior").forEach(link=>{
 });
 
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.style.opacity = 0;
+  setTimeout(() => {
+    document.body.style.transition = "opacity 0.5s";
+    document.body.style.opacity = 1;
+  }, 100);
+});
