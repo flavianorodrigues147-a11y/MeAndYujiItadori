d264f7fc0493c9c1fb10724bc494c21fe8044bad
flavianorodrigues147-a11y <flavianorodrigues147@gmail.com>
2026-03-09 22:05:53 -0300

    mais um

--- js/script.js
+++ js/script.js
@@ -1,29 +1,10 @@
+// script.js
 document.addEventListener("DOMContentLoaded", () => {
+  // Deixa o body invisível até que tudo esteja pronto
+  document.body.style.visibility = "visible";
 
-const paginas = ["index.html","pg1.html","pg2.html","pg3.html", "pg4.html"];
-
-const paginaAtual = window.location.pathname.split("/").pop();
-
-const indexAtual = paginas.indexOf(paginaAtual);
-
-document.querySelectorAll(".proxima").forEach(link=>{
-    if(indexAtual < paginas.length - 1){
-        link.href = paginas[indexAtual + 1];
-    }
-});
-
-document.querySelectorAll(".anterior").forEach(link=>{
-    if(indexAtual > 0){
-        link.href = paginas[indexAtual - 1];
-    }
-});
-
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.style.opacity = 0;
-  setTimeout(() => {
-    document.body.style.transition = "opacity 0.5s";
-    document.body.style.opacity = 1;
-  }, 100);
+  window.addEventListener("load", () => {
+    // Quando CSS e JS estiverem carregados, mostra o conteúdo
+    document.body.style.visibility = "visible";
+  });
 });
